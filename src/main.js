a76d84d00eceb9f8848e4a680d34c8c30752c5fe
Xx-Mohit-xX <75924613+Xx-Mohit-xX@users.noreply.github.com>
2022-12-05 19:09:35 +0530

    Initialize e2e-test

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import "bootstrap";
+import { createApp, h } from "vue";
+import Toast from "vue-toastification";
+import contenteditable from "vue-contenteditable"
+import "vue-toastification/dist/index.css";
+import App from "./App.vue";
+import "./assets/app.scss";
+import { i18n } from "./i18n";
+import { FontAwesomeIcon } from "./icon.js";
+import datetime from "./mixins/datetime";
+import mobile from "./mixins/mobile";
+import socket from "./mixins/socket";
+import theme from "./mixins/theme";
+import publicMixin from "./mixins/public";
+
+import { router } from "./router";
+import { appName } from "./util.ts";
+
+const app = createApp({
+    mixins: [
+        socket,
+        theme,
+        mobile,
+        datetime,
+        publicMixin,
+    ],
+    data() {
+        return {
+            appName: appName
+        }
+    },
+    render: () => h(App),
+})
+
+app.use(router);
+app.use(i18n);
+
+const options = {
+    position: "bottom-right",
+};
+
+app.use(Toast, options);
+app.component("Editable", contenteditable);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
+
+app.mount("#app");
